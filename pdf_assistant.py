2aa25bc44a23dfcd5183cc3cc12e8533fe2bcb7e
nikhil-xyz <sonkusarenikhil@sggs.ac.in>
2025-03-11 22:04:15 +0530

    changes added

--- pdf_assistant.py
+++ pdf_assistant.py
@@ -11,11 +11,12 @@ from dotenv import load_dotenv
 
 load_dotenv()
 os.environ['GROQ_API_KEY'] = os.getenv('GROQ_API_KEY')
+os.environ['MISTRAL_API_KEY'] = os.getenv('MISTRAL_API_KEY')
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
 knowledge_base = PDFUrlKnowledgeBase(
     urls=["https://phi-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf"],
-    vector_db=PgVector2(collection="recipes", db_url=db_url, embedder=MistralEmbedder()),
+    vector_db=PgVector2(collection="recipes", db_url=db_url, embedder=MistralEmbedder(model="mistral-embed")),
 )
 # Load the knowledge base: Comment out after first run
 knowledge_base.load()
